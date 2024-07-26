5ac74337ae00b98d7b95d57fef7cfe23d6ddf90d
nisargbhatsar <visit.nisargd@gmail.com>
2024-07-26 09:03:13 +0530

    project 1
    about my self

---
+++ project upload.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+
+int main()
+{ 
+    printf ("welcome to my profile\n");     
+    printf ( "my name is bhatsar nisarg dineshbhai\n" );
+    
+     printf ("im from patan gujarat " );
+     printf ("im doing b.voc it in shanti bussiness school\n");
+     printf ("i like to work in technical department");
+}
+ 
